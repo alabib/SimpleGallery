81cd23800e92917ba88e17ca37279cf06a9a49c3
Ahmed Labib <a.a.labib@hotmail.com>
2018-09-29 14:02:32 +0200

    Adding Localisation

--- SimpleGallery/SimpleGallery/AppDelegate.swift
+++ SimpleGallery/SimpleGallery/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Localize_Swift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Localize.setCurrentLanguage(Language.English.rawValue) // Change Language
         return true
     }
 
